aeb1d8106eac7699f3e7ae86ae18be8852f6abb1
Mariel Peczak <123342864+mpeczak@users.noreply.github.com>
2023-06-26 15:09:47 -0400

    Update convertNanoToHDF5.py

--- convertNanoToHDF5.py
+++ convertNanoToHDF5.py
@@ -36,16 +36,16 @@ if opt.input == '' or opt.output == '':
 
 varList = [
     'nMuon', 'Muon_pt', 'Muon_eta', 'Muon_phi',
-    'pT_muons', 'eta_muons', 'phi_muons',
-    'nPF', 'PF_pt', 'PF_eta', 'PF_phi', 'PF_pdgId',
-    'PF_charge', 'PF_hcalFraction', 'PF_mass',
-    'PF_dxy', 'PF_dz', 'PF_fromPV', 'PF_puppiWeightNoLep', 'PF_puppiWeight',
+    'nPFCands', 'PFCands_pt', 'PFCands_eta', 'PFCands_phi',
+    'PFCands_pdgId', 'PFCands_charge', 'PFCands_mass',
+    'PFCands_d0', 'PFCands_dz', 'PFCands_fromPV',
+    'PFCands_puppiWeightNoLep', 'PFCands_puppiWeight',
 ]
 
 # event-level variables
 varList_evt = [
     'fixedGridRhoFastjetAll', 'fixedGridRhoFastjetCentralCalo',
-    'PV_npvs', 'PV_npvsGood', 'hasGoodMuon',
+    'PV_npvs', 'PV_npvsGood', 'nMuon'
 ]
 
 varList_mc = [
@@ -53,23 +53,23 @@ varList_mc = [
 ]
 
 d_encoding = {
-    b'PF_charge':{-1.0: 0, 0.0: 1, 1.0: 2},
-    b'PF_pdgId':{-211.0: 0, -13.0: 1, -11.0: 2, 0.0: 3, 1.0: 4, 2.0: 5, 11.0: 6, 13.0: 7, 22.0: 8, 130.0: 9, 211.0: 10},
-    b'PF_fromPV':{0.0: 0, 1.0: 1, 2.0: 2, 3.0: 3}
+    b'PFCands_charge':{-1.0: 0, 0.0: 1, 1.0: 2},
+    b'PFCands_pdgId':{-211.0: 0, -13.0: 1, -11.0: 2, 0.0: 3, 1.0: 4, 2.0: 5, 11.0: 6, 13.0: 7, 22.0: 8, 130.0: 9, 211.0: 10},
+    b'PFCands_fromPV':{0.0: 0, 1.0: 1, 2.0: 2, 3.0: 3}
 }
 
 if not opt.data:
     varList = varList + varList_mc
 varList = varList + varList_evt
     
-upfile = uproot.open(opt.input)
-tree = upfile['Events'].arrays( varList )
+uptree = uproot.open(opt.input + ':Events')
+tree = uptree.arrays( varList )
 
 # general setup
 maxNPF = 4500
 nFeatures = 14
 
-maxEntries = len(tree[b'nPF']) if opt.maxevents==-1 else opt.maxevents
+maxEntries = len(tree['nPFCands']) if opt.maxevents==-1 else opt.maxevents
 # input PF candidates
 X = np.zeros(shape=(maxEntries,maxNPF,nFeatures), dtype=float, order='F')
 # recoil estimators
@@ -84,21 +84,21 @@ print(X.shape)
 # loop over events
 for e in progressbar.progressbar(range(maxEntries), widgets=widgets):
     Leptons = []
-    for ilep in range(min(2, tree[b'hasGoodMuon'][e])):
-        Leptons.append( (tree[b'Muon_pt'][e][ilep], tree[b'Muon_eta'][e][ilep], tree[b'Muon_phi'][e][ilep]) )
+    for ilep in range(min(2, tree['nMuon'][e])):
+        Leptons.append( (tree['Muon_pt'][e][ilep], tree['Muon_eta'][e][ilep], tree['Muon_phi'][e][ilep]) )
         
     # get momenta
     ipf = 0
     ilep = 0
-    for j in range(tree[b'nPF'][e]):
+    for j in range(tree['nPFCands'][e]):
         if ipf == maxNPF:
             break
 
-        pt = tree[b'PF_pt'][e][j]
+        pt = tree['PFCands_pt'][e][j]
         #if pt < 0.5:
         #    continue
-        eta = tree[b'PF_eta'][e][j]
-        phi = tree[b'PF_phi'][e][j]
+        eta = tree['PFCands_eta'][e][j]
+        phi = tree['PFCands_phi'][e][j]
        
         pf = X[e][ipf]
 
@@ -120,29 +120,27 @@ for e in progressbar.progressbar(range(maxEntries), widgets=widgets):
         pf[2] = pt * np.sin(phi)
         pf[3] = eta
         pf[4] = phi
-        pf[5] = tree[b'PF_dxy'][e][j]
-        pf[6] = tree[b'PF_dz'][e][j]
-        pf[7] = tree[b'PF_puppiWeightNoLep'][e][j]
-        pf[8] = tree[b'PF_mass'][e][j]
-        pf[9] = tree[b'PF_hcalFraction'][e][j]
-        pf[10] = tree[b'PF_puppiWeight'][e][j]
+        pf[5] = tree['PFCands_d0'][e][j]
+        pf[6] = tree['PFCands_dz'][e][j]
+        pf[7] = tree['PFCands_puppiWeightNoLep'][e][j]
+        pf[8] = tree['PFCands_mass'][e][j]
+        #pf[9] = tree[b'PF_hcalFraction'][e][j]
+        pf[9] = 0. # variable not found in input data, but also not used in training
+        pf[10] = tree['PFCands_puppiWeight'][e][j]
         # encoding
-        pf[11] = d_encoding[b'PF_pdgId' ][float(tree[b'PF_pdgId' ][e][j])]
-        pf[12] = d_encoding[b'PF_charge'][float(tree[b'PF_charge'][e][j])]
-        pf[13] = d_encoding[b'PF_fromPV'][float(tree[b'PF_fromPV'][e][j])]
+        pf[11] = d_encoding[b'PFCands_pdgId' ][float(tree['PFCands_pdgId' ][e][j])]
+        pf[12] = d_encoding[b'PFCands_charge'][float(tree['PFCands_charge'][e][j])]
+        pf[13] = d_encoding[b'PFCands_fromPV'][float(tree['PFCands_fromPV'][e][j])]
 
     # truth info
-    Y[e][0] = tree[b'pT_muons'][e] * np.cos(tree[b'phi_muons'][e])
-    Y[e][1] = tree[b'pT_muons'][e] * np.sin(tree[b'phi_muons'][e])
-    if not opt.data:
-        Y[e][0] += tree[b'GenMET_pt'][e] * np.cos(tree[b'GenMET_phi'][e])
-        Y[e][1] += tree[b'GenMET_pt'][e] * np.sin(tree[b'GenMET_phi'][e])
+    Y[e][0] += tree['GenMET_pt'][e] * np.cos(tree['GenMET_phi'][e])
+    Y[e][1] += tree['GenMET_pt'][e] * np.sin(tree['GenMET_phi'][e])
 
-    EVT[e][0] = tree[b'fixedGridRhoFastjetAll'][e]
-    EVT[e][1] = tree[b'fixedGridRhoFastjetCentralCalo'][e]
-    EVT[e][2] = tree[b'PV_npvs'][e]
-    EVT[e][3] = tree[b'PV_npvsGood'][e]
-    EVT[e][4] = tree[b'hasGoodMuon'][e]
+    EVT[e][0] = tree['fixedGridRhoFastjetAll'][e]
+    EVT[e][1] = tree['fixedGridRhoFastjetCentralCalo'][e]
+    EVT[e][2] = tree['PV_npvs'][e]
+    EVT[e][3] = tree['PV_npvsGood'][e]
+    EVT[e][4] = tree['nMuon'][e]
 
 with h5py.File(opt.output, 'w') as h5f:
     h5f.create_dataset('X',    data=X,   compression='lzf')
