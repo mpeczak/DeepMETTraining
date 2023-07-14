56543d421aabfd9b7ff3dce3c03850d2ecddab92
Mariel Peczak <123342864+mpeczak@users.noreply.github.com>
2023-07-14 18:57:15 -0400

    Create convertNanoToHDF5v2.py

---
+++ convertNanoToHDF5v2.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+
+import sys
+import uproot
+import numpy as np
+import h5py
+import progressbar
+import os
+import concurrent.futures
+from pathlib import Path
+
+widgets=[
+    progressbar.SimpleProgress(), ' - ', progressbar.Timer(), ' - ', progressbar.Bar(), ' - ', progressbar.AbsoluteETA()
+]
+
+def deltaR(eta1, phi1, eta2, phi2):
+        """ calculate deltaR """
+        dphi = (phi1-phi2)
+        while dphi >  np.pi: dphi -= 2*np.pi
+        while dphi < -np.pi: dphi += 2*np.pi
+        deta = eta1-eta2
+        return np.hypot(deta, dphi)
+
+def conversion(filename):
+   # general setup
+    uptree = uproot.open(filename + ':Events')
+    tree = uptree.arrays( varList )
+    maxEntries = len(tree['nPFCands']) if opt.maxevents==-1 else opt.maxevents
+    
+    # input PF candidates
+    X = np.zeros(shape=(maxEntries,maxNPF,nFeatures), dtype=float, order='F')
+    # recoil estimators
+    Y = np.zeros(shape=(maxEntries,2), dtype=float, order='F')
+    # leptons 
+    XLep = np.zeros(shape=(maxEntries, 2, nFeatures), dtype=float, order='F')
+    # event-level information
+    EVT = np.zeros(shape=(maxEntries,len(varList_evt)), dtype=float, order='F')
+
+    print(X.shape)
+
+    # loop over events
+    for e in progressbar.progressbar(range(maxEntries), widgets=widgets):
+        Leptons = []
+        for ilep in range(min(2, tree['nMuon'][e])):
+            Leptons.append( (tree['Muon_pt'][e][ilep], tree['Muon_eta'][e][ilep], tree['Muon_phi'][e][ilep]) )
+            
+        # get momenta
+        ipf = 0
+        ilep = 0
+        for j in range(tree['nPFCands'][e]):
+            if ipf == maxNPF:
+                break
+
+            pt = tree['PFCands_pt'][e][j]
+            #if pt < 0.5:
+            #    continue
+            eta = tree['PFCands_eta'][e][j]
+            phi = tree['PFCands_phi'][e][j]
+        
+            pf = X[e][ipf]
+
+            isLep = False
+            for lep in Leptons:
+                if deltaR( eta, phi, lep[1], lep[2] )<0.001 and abs(pt/lep[0]-1.0)<0.4:
+                    # pfcand matched to the muon
+                    # fill into XLep instead
+                    pf = XLep[e][ilep]
+                    ilep += 1
+                    Leptons.remove(lep)
+                    break
+            if not isLep:
+                ipf += 1
+                    
+            # 4-momentum
+            pf[0] = pt
+            pf[1] = pt * np.cos(phi)
+            pf[2] = pt * np.sin(phi)
+            pf[3] = eta
+            pf[4] = phi
+            pf[5] = tree['PFCands_d0'][e][j]
+            pf[6] = tree['PFCands_dz'][e][j]
+            pf[7] = tree['PFCands_puppiWeightNoLep'][e][j]
+            pf[8] = tree['PFCands_mass'][e][j]
+            #pf[9] = tree[b'PF_hcalFraction'][e][j]
+            pf[9] = 0. # variable not found in input data, but also not used in training
+            pf[10] = tree['PFCands_puppiWeight'][e][j]
+            # encoding
+            pf[11] = d_encoding[b'PFCands_pdgId' ][float(tree['PFCands_pdgId' ][e][j])]
+            pf[12] = d_encoding[b'PFCands_charge'][float(tree['PFCands_charge'][e][j])]
+            pf[13] = d_encoding[b'PFCands_fromPV'][float(tree['PFCands_fromPV'][e][j])]
+
+        # truth info
+        Y[e][0] += tree['GenMET_pt'][e] * np.cos(tree['GenMET_phi'][e])
+        Y[e][1] += tree['GenMET_pt'][e] * np.sin(tree['GenMET_phi'][e])
+
+        EVT[e][0] = tree['fixedGridRhoFastjetAll'][e]
+        EVT[e][1] = tree['fixedGridRhoFastjetCentralCalo'][e]
+        EVT[e][2] = tree['PV_npvs'][e]
+        EVT[e][3] = tree['PV_npvsGood'][e]
+        EVT[e][4] = tree['nMuon'][e]
+    
+    return X,Y,EVT,XLep
+
+
+if __name__ == '__main__':
+    usage = 'usage: %prog [options]'
+    import optparse
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-i', '--input', dest='input', help='input file', default='', type='string')
+    parser.add_option('-o', '--output', dest='output', help='output file', default='', type='string')
+    parser.add_option("-N", "--maxevents", dest='maxevents', help='max number of events', default=-1, type='int')
+    parser.add_option("--data", dest="data", action="store_true", default=False, help="input is data. The default is MC")
+    (opt, args) = parser.parse_args()
+
+    '''if opt.input == '' or opt.output == '':
+    sys.exit('Need to specify input and output files!')'''
+
+
+    varList = [
+    'nMuon', 'Muon_pt', 'Muon_eta', 'Muon_phi',
+    'nPFCands', 'PFCands_pt', 'PFCands_eta', 'PFCands_phi',
+    'PFCands_pdgId', 'PFCands_charge', 'PFCands_mass',
+    'PFCands_d0', 'PFCands_dz', 'PFCands_fromPV',
+    'PFCands_puppiWeightNoLep', 'PFCands_puppiWeight',
+    ]
+
+    # event-level variables
+    varList_evt = [
+        'fixedGridRhoFastjetAll', 'fixedGridRhoFastjetCentralCalo',
+        'PV_npvs', 'PV_npvsGood', 'nMuon'
+    ]
+
+    varList_mc = [
+        'GenMET_pt', 'GenMET_phi',
+    ]
+
+    d_encoding = {
+        b'PFCands_charge':{-1.0: 0, 0.0: 1, 1.0: 2},
+        b'PFCands_pdgId':{-211.0: 0, -13.0: 1, -11.0: 2, 0.0: 3, 1.0: 4, 2.0: 5, 11.0: 6, 13.0: 7, 22.0: 8, 130.0: 9, 211.0: 10},
+        b'PFCands_fromPV':{0.0: 0, 1.0: 1, 2.0: 2, 3.0: 3}
+    }
+
+    if not opt.data:
+        varList = varList + varList_mc
+    varList = varList + varList_evt
+
+    maxNPF = 4500
+    nFeatures = 14
+    
+    nworkers = 8
+
+    #input_directory = '/hildafs/home/peczak/DeepMETTraining/test/'
+    input_directory = '/export/home/phys/mpeczak/DeepMETTraining/split_nano/'
+    output_directory = '/export/home/phys/mpeczak/DeepMETTraining/outputs/'
+    #output_directory = '/hildafs/projects/phy230010p/peczak/testdirectory/'
+    directory = os.fsencode(input_directory)
+
+#tree_dict = {}
+filenames = []
+for file in os.listdir(directory):
+
+    name = os.path.basename(file)
+    filename = input_directory + name.decode("utf-8")
+    filenames.append(filename)
+    #uptree = uproot.open( filename + ':Events')
+    #tree = uptree.arrays( varList )
+    #maxEntries = len(tree['nPFCands']) if opt.maxevents==-1 else opt.maxevents
+    #print (tree)
+
+    #tree_dict[filename] = tree
+
+
+#print(tree_dict)
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=nworkers) as executor:
+    futures = set()
+
+    futures.update(executor.submit(conversion, filename) for filename in filenames)
+    
+    #if(len(futures)==0): 
+        #continue
+    
+    '''for k,v in tree_dict:
+        futures.update(executor.submit(conversion(v), k, output_directory+k))
+        if(len(futures)==0): 
+            continue'''   
+    try:
+        total = len(futures)
+        processed = 0
+        while len(futures) > 0:
+            finished = set(job for job in futures if job.done())
+            for job in finished:
+                X,Y,EVT,XLep = job.result()
+                with h5py.File(output_directory+filename, 'w') as h5f:
+                    h5f.create_dataset('X',    data=X,   compression='lzf')
+                    h5f.create_dataset('Y',    data=Y,   compression='lzf')
+                    h5f.create_dataset('EVT',  data=EVT, compression='lzf')
+                    h5f.create_dataset('XLep', data=XLep, compression='lzf')
+                    #x y evt xlep arrays = job.result, then create hdf5'''
+                print (X,Y,EVT,XLep)
+                processed += 1
+            futures -= finished
+        del finished
+    except KeyboardInterrupt:
+        print("Ok quitter")
+        for job in futures: job.cancel()
+    except:
+        for job in futures: job.cancel()
+        raise
+    print (X,Y,EVT,XLep)
+
+    '''
+    with h5py.File(opt.output, 'w') as h5f:
+    h5f.create_dataset('X',    data=X,   compression='lzf')
+    h5f.create_dataset('Y',    data=Y,   compression='lzf')
+    h5f.create_dataset('EVT',  data=EVT, compression='lzf')
+    h5f.create_dataset('XLep', data=XLep, compression='lzf')
+    '''
